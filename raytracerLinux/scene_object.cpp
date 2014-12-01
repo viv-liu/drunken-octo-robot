a99ab4a38cc7b280413800528a83c470040c3d3c
Soon Chee Loong <c4soonch@cdf.toronto.edu>
2014-12-01 16:43:57 -0500

    no improvement, but attempted multiplying specular component which doesn't look as good, so commented out

--- raytracerLinux/scene_object.cpp
+++ raytracerLinux/scene_object.cpp
@@ -110,7 +110,6 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel, const Mat
 }
 
 
-
 bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 		const Matrix4x4& modelToWorld ) {
 	// TODO: implement intersection code for UnitSphere, which is centred 
@@ -119,46 +118,55 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	// Your goal here is to fill ray.intersection with correct values
 	// should an intersection occur.  This includes intersection.point, 
 	// intersection.normal, intersection.none, intersection.t_value.   
+
 	//
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
 	
 	Ray3D ObjectRay = Ray3D(worldToModel * ray.origin, worldToModel * ray.dir);
 
+
 	double a = pow(ObjectRay.dir[0], 2) + pow(ObjectRay.dir[1], 2) + pow(ObjectRay.dir[2], 2);
 	double b = 2 * (ObjectRay.origin[0] * ObjectRay.dir[0] +
 					ObjectRay.origin[1] * ObjectRay.dir[1] +
 					ObjectRay.origin[2] * ObjectRay.dir[2]);
+
 	double c = pow(ObjectRay.origin[0], 2) + 
 			   pow(ObjectRay.origin[1], 2) + 
 			   pow(ObjectRay.origin[2], 2) -1;
 
 	double discriminant = pow(b, 2) - 4 * a * c;
 
+
 	if(discriminant < 0) { // No intersection
 		return false;
 
 	} else {
+
 		double t_1 = (-1 * b + sqrt(discriminant)) / (2 * a);
 		double t_2 = (-1 * b - sqrt(discriminant)) / (2 * a);
 		double t_value = 0;
 
 		if ( t_2 < 0 && t_1 < 0)
+
 			return false;
 		else if ( t_1 < 0 )
 			t_value = t_2;
 		else if ( t_2 < 0 )
 			t_value = t_1;
+
 		else
 			t_value = fmin(t_1, t_2);
 
 		// If the ray already has an intersection at an earlier t_value, return but don't update
 		// ray.intersection fields
+
 		if(ray.intersection.none == false) {
 			if(t_value > ray.intersection.t_value) {
 				return true;
 			}
 		}
+
 
 		double x = ObjectRay.origin[0] + ObjectRay.dir[0]*t_value;
 		double y = ObjectRay.origin[1] + ObjectRay.dir[1]*t_value;
@@ -169,21 +177,26 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 		ray.intersection.point = modelToWorld * Point3D(x, y, z);
 		ray.intersection.normal = transNorm(worldToModel, intersection);
 		ray.intersection.normal.normalize();		
+
 		ray.intersection.none = false;
 
 	//	std::cout << "Normal ";
 	//		std::cout << ray.intersection.normal[0];
 	//		std::cout << ray.intersection.normal[1];
+
 	//		std::cout << ray.intersection.normal[2];
 	//		std::cout <<"\n";
 	//	std::cout << "Point ";
 	//		std::cout << intersection[0];
 	//		std::cout << intersection[1];
+
 	//		std::cout << intersection[2];
 	//		std::cout <<"\n";
 		return true;
 	}
 }
+// */
+
 
 
 
